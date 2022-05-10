d370ee331512bb10b1811d0a499a0c6670610485
vakees14 <vakees.ilamaran@gmail.com>
2022-05-10 23:21:02 +0200

    Adding the right pipelin

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
 resource "docker_container" "deploy" {
   name  = "manager"
   image = airbus-release.latest
