c96aeeae8e095057e6a4bf489f56d7bd7e3f705d
vakees14 <vakees.ilamaran@gmail.com>
2022-05-10 23:29:19 +0200

    asdasdasd

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 resource "docker_container" "deploy" {
   name  = "manager"
-  image = airbus-release.latest
+  image = "airbus-release:latest"
   ports {
       internal = 5000
       external = 5000
